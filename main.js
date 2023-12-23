57862876621b381b8f1762fe7393fe35d26df5e9
Soham pal <154463867+soham123-undertaker@users.noreply.github.com>
2023-12-23 19:53:58 +0530

    Add files via upload
    let const and var code

---
+++ main.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "spal9384@gmail.com"
+var accountPassword = "123456"
+accountCity = "kolkata"
+
+// accountId = 2  //not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "2121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
