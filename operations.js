357e1e2eef6cf78d332d5891176b0762b6e898b1
Soham pal <spal97481@gmail.com>
2023-12-24 13:19:55 +0530

    Javascript codes about operations

---
+++ operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//Basic Arithmetic operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%2);
+
+//Concatenating strings
+let str1 = "Soham"
+let str2 = " Pal"
+let str3 = str1 + str2
+console.log(str3);
+
+//Confusion in Conversions
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
