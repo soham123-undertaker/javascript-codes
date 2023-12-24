bf60057779f33a4efa5203c46c5b60acab8a65bf
Soham pal <spal97481@gmail.com>
2023-12-24 10:46:53 +0530

    Learn about datatypes

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+"use strict";   // treat all JS code as newer version
+
+//alert(3+3)  //we are using node js, not browser
+
+console.log(3
+    +
+    3);  //Code readability should be high
+
+console.log("Soham");
+
+let name = "SOham"
+let age = 18
+let isLoggedIn = true  //or false
+let state;
+
+console.table([name,age,isLoggedIn,state])
+
+//primitive datatypes :-
+//number  => 2 to power 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+//user defined data type
+//Object  
+
+console.log(typeof "soham");
+console.log(typeof age);
+console.log(typeof null);  //Object
+console.log(typeof undefined);  //undefined
